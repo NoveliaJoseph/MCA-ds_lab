b57367cf18ef25fffac054a47b9f7a4aaafea83d
NoveliaJoseph <156586616+NoveliaJoseph@users.noreply.github.com>
2025-07-16 14:48:29 +0530

    Update GlobalVariables.cpp

--- GlobalVariables.cpp
+++ GlobalVariables.cpp
@@ -7,6 +7,7 @@ void insert(int e) {
         printf("Error: Array is full\n");
     } else {
         a[++pos] = e;
+        printf("Inserted: %d\n", e);
     }
 }
 
@@ -39,58 +40,18 @@ void sort() {
     for (int i = 0; i <= pos; i++) {
         for (int j = 0; j < pos - i; j++) {
             if (a[j] > a[j + 1]) {
-                // Swap
                 int temp = a[j];
                 a[j] = a[j + 1];
                 a[j + 1] = temp;
             }
         }
     }
-    printf("Array sorted.\n");
+    printf("Array sorted in ascending order.\n");
+    display(); // Display sorted array
 }
 
 int menu() {
     int ch;
-    printf("\nMenu:\n");
+    printf("\n--- Menu ---\n");
     printf("1. Insert\n");
-    printf("2. Deletee\n");
-    printf("3. Display\n");
-    printf("4. Exit\n");
-    printf("5. Sort\n");
-    printf("Enter your choice: ");
-    scanf("%d", &ch);
-    return ch;
-}
-
-void processArray() {
-    int ch;
-    for (ch = menu(); ch != 4; ch = menu()) {
-        switch (ch) {
-            case 1: {
-                int val;
-                printf("Enter the value to be inserted: ");
-                scanf("%d", &val);
-                insert(val);
-                break;
-            }
-            case 2:
-                deletee();
-                break;
-            case 3:
-                display();
-                break;
-            case 5:
-                sort();
-                break;
-            default:
-                printf("Error: Wrong choice\n");
-                break;
-        }
-    }
-}
-
-int main() {
-    processArray();
-    return 0;
-}
-
+    print
